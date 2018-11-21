b89f1cad585e065d7427495dac98c3a76fea87d2
Xeon <xeon.hash@kakaomobility.com>
2018-11-21 16:22:53 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # UIScaleView
+
+##Overview
+
+UIScaleView is inspired by UILabel's adjustsFontSizeToFitWidth property. 
+
+When contents size exceed it's own frame size, label deals with it by scaling font size.
+
+When make complicated layout of view, restricted bounding width or height make problem. if you deal with it only labels' adjustsFontSizeToFitWidth property, the rational frame of layout will be collapsed.
+
+UIScaleView deal with it by scaling overall layout like it is one image.
+
+when a contents view whose minimum width is 200 is inserted into space where width is constrainted to 100, it can inserted into space by scaling 50%. and height is calculated by scaling ratio, so height should be open constrainted.
+
+it's opposite case can be also true. where maximum width of contents is defined. it can be deal with it by scaling view to scaling to over 100%.
+
+
+##matching properties
+
+| UIScaleView | UILabel | etc |
+|----|----|----|
+| defineSmallerScaleToFitWidth | adjustsFontSizeToFitWidth  | |
+| defineBiggerScaleToFitWidth | | added |
+| minimumScaleFactor | minimumScaleFactor |
+| maximumScaleFactor | | added |
+
+
+
+
+
---
+++ UIScaleView.swift
@@ -0,0 +1,137 @@
+//
+//  UIScaleView.swift
+//  UIScaleView
+//
+//  Created by Xeon on 21/11/2018.
+//  Copyright © 2018 Yu Tae Yeon. All rights reserved.
+//
+
+import UIKit
+
+final class UIScaleView: UIView {
+    
+    private var _minimumScaleFactor: CGFloat = CGFloat(0) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    private var _maximumScaleFactor: CGFloat = CGFloat.greatestFiniteMagnitude {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    var defineSmallerScaleToFitWidth: Bool = true
+    
+    var defineBiggerScaleToFitWidth: Bool = true
+    
+    var minimumScaleFactor: CGFloat {
+        set {
+            guard newValue <= 1.0, newValue >= 0.0 else { return }
+            self._minimumScaleFactor = newValue
+        }
+        
+        get {
+            return _minimumScaleFactor
+        }
+    }
+    
+    var maximumScaleFactor: CGFloat {
+        set {
+            guard newValue >= 1.0 else { return }
+            self._maximumScaleFactor = newValue
+        }
+        
+        get {
+            return _maximumScaleFactor
+        }
+    }
+    
+    enum SizeFittingViewAxis {
+        case horizontal
+        case vertical
+    }
+    
+    var axis: SizeFittingViewAxis = SizeFittingViewAxis.horizontal {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    weak var contentsView: UIView! {
+        didSet {
+            oldValue.removeFromSuperview()
+            self.addSubview(contentsView)
+            self.setNeedsLayout()
+        }
+    }
+    
+    func constraintView(targetSize: CGSize) {
+        let contentsSize = contentsView.systemLayoutSizeFitting(targetSize)
+        
+        let targetRatio: CGFloat
+        switch self.axis {
+        case .horizontal:
+            targetRatio = targetSize.width/contentsSize.width
+        case .vertical:
+            targetRatio = targetSize.height/contentsSize.height
+        }
+        
+        let constraintedRatio = min(max(self._minimumScaleFactor, targetRatio), self._maximumScaleFactor)
+        
+        let constraintedSize: CGSize
+        switch self.axis {
+        case .horizontal:
+            constraintedSize = CGSize(width: targetSize.width / constraintedRatio, height: contentsSize.height)
+        case .vertical:
+            constraintedSize = CGSize(width: contentsSize.width, height: targetSize.height / constraintedRatio)
+        }
+        
+        let v: CGFloat
+        
+        switch self.axis {
+        case .horizontal:
+            v = targetSize.width / constraintedSize.width
+        case .vertical:
+            v = targetSize.height / constraintedSize.height
+        }
+        
+        contentsView.bounds = CGRect(origin: CGPoint.zero, size: constraintedSize)
+        contentsView.center = CGPoint(x: (constraintedSize.width * v)/2, y: (constraintedSize.height * v)/2)
+        contentsView.transform = CGAffineTransform(scaleX: v, y: v)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if let _ = contentsView {
+            self.constraintView(targetSize: self.bounds.size)
+        }
+        
+        self.invalidateIntrinsicContentSize()
+    }
+    
+    override func systemLayoutSizeFitting(_ targetSize: CGSize) -> CGSize {
+        let contentsSize = contentsView.systemLayoutSizeFitting(targetSize)
+        
+        switch self.axis {
+        case .horizontal:
+            let targetRatio = targetSize.width/contentsSize.width
+            
+            let constraintedRatio = min(max(self._minimumScaleFactor, targetRatio), self._maximumScaleFactor)
+            
+            return CGSize(width: targetSize.width, height: contentsSize.height * constraintedRatio)
+        case .vertical:
+            let targetRatio = targetSize.height/contentsSize.height
+            
+            let constraintedRatio = min(max(self._minimumScaleFactor, targetRatio), self._maximumScaleFactor)
+            
+            return CGSize(width: contentsSize.width * constraintedRatio, height: targetSize.height)
+        }
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return self.systemLayoutSizeFitting(bounds.size)
+    }
+}
